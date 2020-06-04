7995489d0823eca5be39b57a2e4bd687d64c92a5
MacMgy <macmgy@yandex.ru>
2020-06-04 17:18:19 +0700

    del generate file

--- VDOM XML Generator/src/xmlBuilder.js
+++
@@ -1,8 +0,0 @@
-export function BuildObject(obj) {
-    let str = "<" + obj.tag + " ";
-    obj.attributes.forEach(function (element) {
-        str += element.name + "=\"" + element.value + "\" ";
-    });
-    str += "/>";
-    return str;
-}
